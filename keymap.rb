c8c1e4ce9694ec96c8d8c568d7512020b8384d60
swanmatch <swan_match@yahoo.co.jp>
2023-05-07 17:29:13 +0900

    fix keymap

--- keymap.rb
+++ keymap.rb
@@ -1,3 +1,5 @@
+require "consumer_key"
+
 kbd = Keyboard.new
 
 kbd.init_direct_pins([
@@ -5,12 +7,44 @@ kbd.init_direct_pins([
   2,  1,  7,  0
 ])
 
-
-# default layer should be added at first
 kbd.add_layer :default, %i[
-  KC_1  KC_R KC_U  KC_3
-  KC_2  KC_B KC_Y  KC_4
+  COPY           RAILS    CONSOLE    PASTE
+  BKSP_LEFT      BUNDLE   RAKE       DEL_RIGHT
 ]
+
+kbd.add_layer :right, %i[
+  KC_MUTE        KC_VOLD  KC_VOLU    KC_MPLY
+  ADJUST_BOOT    KC_MPRV  KC_MNXT    DEL_RIGHT
+]
+
+kbd.add_layer :left, %i[
+  KC_WHOM        KC_BRID  KC_BRIU    KC_CALC
+  BKSP_LEFT      KC_WBAK  KC_WFWD    ADJUST_BOOT
+]
+
+kbd.add_layer :adjust, %i[
+  RGB_RMOD       RGB_SAI  RGB_VAI    RGB_MOD
+  ADJUST_BOOT    RGB_SAD  RGB_VAD    ADJUST_BOOT
+]
+
+# Ctrl and others
+kbd.define_mode_key :SELALL, [ %i[KC_RCTL KC_A], nil, 200, 200 ]
+kbd.define_mode_key :SAVE,   [ %i[KC_RCTL KC_S], nil, 200, 200 ]
+kbd.define_mode_key :UNDO,   [ %i[KC_RCTL KC_Z], nil, 200, 200 ]
+kbd.define_mode_key :REDO,   [ %i[KC_RCTL KC_Y], nil, 200, 200 ]
+kbd.define_mode_key :COPY,   [ %i[KC_RCTL KC_C], nil, 200, 200 ]
+kbd.define_mode_key :PASTE,  [ %i[KC_RCTL KC_V], nil, 200, 200 ]
+
+# when tap any key, and Hold move layer
+kbd.define_mode_key :BKSP_LEFT,    [ :KC_LEFT,  :left,  200, 200 ]
+kbd.define_mode_key :DEL_RIGHT,    [ :KC_RIGHT, :right,  200, 200 ]
+kbd.define_mode_key :ADJUST_BOOT, [ Proc.new { kbd.bootsel! }, :adjust,  200, 200 ]
+
+# macro
+kbd.define_mode_key :BUNDLE, [ Proc.new { kbd.macro("bundle exec ", []) }, nil, 200, nil]
+kbd.define_mode_key :RAILS, [ Proc.new { kbd.macro("rails s") }, nil, 200, nil]
+kbd.define_mode_key :CONSOLE, [ Proc.new { kbd.macro("rails c") }, nil, 200, nil]
+kbd.define_mode_key :RAKE, [ Proc.new { kbd.macro("rake", []) }, nil, 200, nil]
 
 rgb = RGB.new(
   26,
